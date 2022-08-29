c2a68a23aca05413f31562ef23404d1f97955a8f
Riickard <rickard.englund10@outlook.com>
2022-08-29 10:48:19 +0200

    basicLayout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
+import { useState } from "react";
 import Login from "./components/pages/Login";
-
-
+import { Box } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Playlist from "./components/pages/Playlist";
+import Library from "./components/pages/Library";
+import Home from "./components/pages/Home";
+import SideNav from "./components/SideNav/SideNav";
+import MobileNav from "./components/MobilNav/MobileNav";
+import Player from "./components/Player/Player";
 function App() {
-
+  const [isPlayerReady, setSsPlayerReady] = useState(true)
+  const [token, setToken] = useState(false);
 
   return (
-    <div>
-      <h1>Spotify!</h1>
-    </div>
+    <Box className="App">
+      {token ? (
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Box sx={{ flex: 1, overflowY: "auto", display: "flex" }}>
+            <SideNav />
+            <Routes>
+              <Route path="/playlist/:id" element={<Playlist />} />
+              <Route path="/library" element={<Library />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Box>
+          {isPlayerReady && <Player />}
+          <MobileNav />
+        </Box>
+      ) : (
+        <Routes>
+          <Route path="*" element={<Login />} />
+        </Routes>
+      )}
+    </Box>
   );
 }
 
---
+++ src/components/pages/Library.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Box, List, Typography } from "@mui/material";
+
+
+const Library = () => {
+  return (
+    <Box
+      id="Library"
+      px={3}
+      sx={{
+        display: { xs: "flex", md: "none" },
+        bgcolor: "background.default",
+        flex: 1,
+        flexDirection: "column",
+        overflowY: "auto",
+      }}
+    >
+      <Typography
+        py={3}
+        variant="h2"
+        fontWeight="bold"
+        sx={{ color: "text.primary", fontSize: 30 }}
+      >
+        Ditt bibliotek
+      </Typography>
+      <List>List to be seen</List>
+    </Box>
+  );
+};
+
+
+export default Library;
