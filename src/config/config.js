22a2003bf041440f9b40502edceb2ed6c4c50bdf
Riickard <rickard.englund10@outlook.com>
2022-08-29 10:25:45 +0200

    basic config and login UI

---
+++ src/config/config.js
@@ -0,0 +1,32 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+export const clientId = 'd5c0f95ce6054352b55c2de78e9f17a1';
+export const liveURL = 'http://localhost:3000/';
+export const devURL = 'http://localhost:3000/';
+export const redirectURL = liveURL;
+export const scopes = [
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+  'playlist-read-private',
+  'playlist-modify-private',
+  'app-remote-control',
+  'streaming',
+  'user-read-email',
+  'user-read-private',
+  'user-library-modify',
+  'user-library-read',
+  'user-top-read',
+  'user-read-playback-position',
+  'ugc-image-upload',
+  'user-modify-playback-state',
+  'user-read-playback-state',
+  'user-read-currently-playing',
+  'user-follow-modify',
+  'user-follow-read',
+  'user-read-recently-played',
+];
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectURL}&scope=${scopes.join(
+  '%20'
+)}&response_type=token&show_dialog=true`;
+
+
