3d65a68eae8ee62b9b0b57524ef260c5b42af9a6
Riickard <rickard.englund10@outlook.com>
2022-09-07 11:39:22 +0200

    added playlistitem to library

---
+++ src/components/PlaylistItem/PlaylistItem.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  ListItem,
+  ListItemButton,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  Skeleton,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const PlaylistItem = ({ name, images, id, isLoading }) => {
+  const navigate = useNavigate();
+
+  if (isLoading) {
+    return (
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemAvatar sx={{ marginRight: "16px" }}>
+            <Skeleton variant="rectangular" width={60} height={60} />
+          </ListItemAvatar>
+          <Skeleton variant="text" width={150} height={20} />
+        </ListItemButton>
+      </ListItem>
+    );
+  }
+
+  return (
+    <ListItem disablePadding>
+      <ListItemButton onClick={() => navigate(`/playlist/${id}`)}>
+        <ListItemAvatar sx={{ marginRight: "16px" }}>
+          <Avatar
+            src={images?.[0]?.url}
+            variant="square"
+            width={60}
+            height={60}
+          />
+        </ListItemAvatar>
+        <ListItemText primary={name} sx={{ color: "text.primary" }} />
+      </ListItemButton>
+    </ListItem>
+  );
+};
+
+export default PlaylistItem;
